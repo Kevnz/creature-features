520f6b80d52e2e37a4bc72df76c4773810e0b064
Kevin <kevin.isom@gmail.com>
2021-07-21 11:29:48 +1200

    fix

--- test/rules.test.js
+++ test/rules.test.js
@@ -2,7 +2,7 @@ describe('Feature based on rules', () => {
   it('should when passed a parameter evaluate the rule and return true or false', () => {
     const creature = require('../index')
     const locationFeatures = creature({
-      location: './test/features/rules'
+      location: './test/features/rules',
     })
 
     expect(locationFeatures.EmailBasedFeature('test@example.com')).toBe(true)
@@ -12,19 +12,19 @@ describe('Feature based on rules', () => {
   it('should when passed an object evaluate the rule and return true or false', () => {
     const creature = require('../index')
     const locationFeatures = creature({
-      location: './test/features/rules'
+      location: './test/features/rules',
     })
 
     expect(
       locationFeatures.ObjectPassedFeature({
         email: 'test@example.com',
-        role: 'admin'
+        role: 'admin',
       })
     ).toBe(true)
     expect(
       locationFeatures.ObjectPassedFeature({
         email: 'nope@example.com',
-        role: 'user'
+        role: 'user',
       })
     ).toBe(false)
   })
@@ -32,20 +32,20 @@ describe('Feature based on rules', () => {
   it('should when passed an object evaluate the rule and return false if missing parameters', () => {
     const creature = require('../index')
     const locationFeatures = creature({
-      location: './test/features/rules'
+      location: './test/features/rules',
     })
 
     expect(
       locationFeatures.ObjectPassedFeature({
         email: 'test@example.com',
-        role: 'admin'
+        role: 'admin',
       })
     ).toBe(true)
 
     expect(
       locationFeatures.ObjectPassedFeature({
         email: 'nope@example.com',
-        role: 'user'
+        role: 'user',
       })
     ).toBe(false)
   })
